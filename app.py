f193219258752e0f43f67482b789c45fb82b5360
Gabriel Fonseca <gabriel@ethx.com.br>
2023-05-10 16:08:33 -0300

    Atualização das validações com o uso do validate.

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ import re
 from datetime import datetime, timedelta
 from flask import Flask, request, jsonify, make_response
 from flask_sqlalchemy import SQLAlchemy
-from marshmallow import fields, Schema, ValidationError
-from marshmallow.validate import Length
+from marshmallow import fields, Schema, ValidationError, validates
+from marshmallow.validate import Length, And, Regexp, Range
 from flask_migrate import Migrate
 
 
@@ -11,9 +11,9 @@ app = Flask(__name__)
 # os.environ['FLASK_APP'] = 'app'  # a princípio não é necessário
 
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://gabriel:senha@db:3306/desafioflex'  # quando usa docker compose/mysql
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://gabriel:senha@127.0.0.1:3306/desafioflex'  # quando usa mysql
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://gabriel:senha@127.0.0.1:3306/desafioflex'  # quando usa mysql
 app.app_context().push()  # ver porque é necessário
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///certificates.sqlite3'  # quando usa sqlite3
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///certificates.sqlite3'  # quando usa sqlite3
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)  # sql-migrate
 
@@ -56,14 +56,41 @@ def test_validate(value):
         raise ValidationError('Quantity must be greater than 0.')
 
 
+class GroupSchema(Schema):
+
+    id = fields.Integer(dump_only=True)
+    created_at = fields.DateTime(dump_only=True)
+    updated_at = fields.DateTime(dump_only=True)
+    name = fields.String(required=True, validate=Length(max=30))
+    description = fields.String(required=True, validate=Length(max=255))
+
+    class Meta:
+        fields = ('id', 'name', 'description', 'created_at', 'updated_at')
+
+
 class CertificateSchema(Schema):
-    # username = fields.Str(required=True, validate=validate.Length(max=4, error='WEojwef'))
-    username = fields.Str(validate=test_validate)
-    groups = fields.Method("get_groups_ids")
 
+    def get_groups_ids(self, value):
+        return [group.id for group in value.groups]
 
-    def get_groups_ids(self, certificate):
-        return [group.id for group in certificate.groups]
+    def load_groups_ids(self, value):
+        return list(value)
+
+    id = fields.Integer(dump_only=True)
+    created_at = fields.DateTime(dump_only=True)
+    updated_at = fields.DateTime(dump_only=True)
+    expiration_at = fields.DateTime()
+    # username = fields.Str(required=True, validate=Length(max=30))
+    username = fields.Str(required=True, validate=And(Length(max=255), Regexp(regex=r'^[a-zA-Z0-9]+$')))
+    name = fields.Str(required=True, validate=Length(max=255))
+    expiration = fields.Integer(required=True, validate=Range(10, 3650))
+    groups = fields.Method("get_groups_ids", "load_groups_ids")  # esse funciona, quando não ta validando (.load(data, INCLUDE))
+
+    # @validates("username")
+    # def validate_username(self, value):
+    #     pattern = r'^[a-zA-Z0-9]+$'
+    #     if not re.match(pattern, value):
+    #         raise ValidationError("the username must only contain alphanumeric characters")
 
     class Meta:
         fields = (
@@ -80,20 +107,8 @@ class CertificateSchema(Schema):
         )
 
 
-class GroupSchema(Schema):
-
-    id = fields.Integer(dump_only=True)
-    created_at = fields.DateTime(dump_only=True)
-    updated_at = fields.DateTime(dump_only=True)
-    name = fields.String(required=True, validate=Length(max=30))
-    description = fields.String(required=True, validate=Length(max=255))
-
-    class Meta:
-        fields = ('id', 'name', 'description', 'created_at', 'updated_at')
-
-
-certificate_schema = CertificateSchema()
-certificates_schema = CertificateSchema(many=True)
+# certificate_schema = CertificateSchema()
+# certificates_schema = CertificateSchema(many=True)
 # group_schema = GroupSchema()
 # groups_schema = GroupSchema(many=True)
 
@@ -123,11 +138,9 @@ def list_certificates():
             certificates_query = certificates_query.order_by(Certificate.name.asc())
 
         certificates = certificates_query.all()
+        certificates_serialized = CertificateSchema(many=True).dump(certificates)
 
-        schema = CertificateSchema(many=True)
-        certificates_json = schema.dump(certificates)
-
-        return jsonify(certificates_json)
+        return jsonify(certificates_serialized)
 
 
 @app.route('/grupos', methods=['GET'])
@@ -141,28 +154,37 @@ def list_groups():
 @app.route('/certificados', methods=['POST'])
 def create_certificate():
     with app.app_context():
-        certificate_data = request.json
+
+        try:
+            # certificate_data = CertificateSchema().load(request.json, unknown=INCLUDE)
+            certificate_data = CertificateSchema().load(request.json)
+        except ValidationError as err:
+            print(f"Erro no create_certificate: {err.messages}")
+            print(f"Campos ok no create_certificate: {err.data}")
+            return make_response(jsonify(err.messages), 400)
+
+        # certificate_data = request.json
         group_ids = [int(group_id) for group_id in certificate_data['groups']]
         groups = Group.query.filter(Group.id.in_(group_ids)).all()
 
-        username = certificate_data['username']
-        pattern = r'^[a-zA-Z0-9]+$'
-        if not re.match(pattern, username):
-            return make_response(jsonify({'message': 'the username must only contain alphanumeric characters'}), 400)
+        # username = certificate_data['username']
+        # pattern = r'^[a-zA-Z0-9]+$'
+        # if not re.match(pattern, username):
+        #     return make_response(jsonify({'message': 'the username must only contain alphanumeric characters'}), 400)
 
-        if len(username) > 30:
-            return make_response(jsonify({'message': 'the username must not exceed 30 characters'}), 400)
+        # if len(username) > 30:
+        #     return make_response(jsonify({'message': 'the username must not exceed 30 characters'}), 400)
 
-        existing_certificate = Certificate.query.filter_by(username=username).first()
-        if existing_certificate:
-            return make_response(jsonify({'message': 'username already exists.'}), 400)
+        # existing_certificate = Certificate.query.filter_by(username=username).first()
+        # if existing_certificate:
+        #     return make_response(jsonify({'message': 'username already exists.'}), 400)
 
-        expiration = certificate_data['expiration']
-        if isinstance(expiration, int):
-            if expiration > 3650 or expiration < 10:
-                return make_response(jsonify({'message': 'the expiration field must be an integer between 10 and 3650'}), 400)
-        else:
-            return make_response(jsonify({'message': 'the expiration field must be an integer'}), 400)
+        # expiration = certificate_data['expiration']
+        # if isinstance(expiration, int):
+        #     if expiration > 3650 or expiration < 10:
+        #         return make_response(jsonify({'message': 'the expiration field must be an integer between 10 and 3650'}), 400)
+        # else:
+        #     return make_response(jsonify({'message': 'the expiration field must be an integer'}), 400)
 
         certificate = Certificate(
             name=certificate_data['name'],
@@ -176,14 +198,13 @@ def create_certificate():
         db.session.add(certificate)
         db.session.commit()
 
-        certificate_serialized = certificate_schema.dump(certificate)
+        certificate_serialized = CertificateSchema().dump(certificate)
         return jsonify(certificate_serialized), 201
 
 
 @app.route('/grupos', methods=['POST'])
 def create_group():
     with app.app_context():
-
         try:
             group_data = GroupSchema().load(request.json)
         except ValidationError as err:
@@ -310,5 +331,5 @@ def delete_group(group_id):
 
 
 if __name__ == "__main__":
-    # db.create_all() # necessário adicionar se for utilizar o SQLite3
+    db.create_all() # necessário adicionar se for utilizar o SQLite3
     app.run(debug=True)
