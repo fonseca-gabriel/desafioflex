d27f92ba65c0ee5d7905f0a5be3f67fdb9cd63ba
Gabriel Fonseca <gabriel@ethx.com.br>
2023-05-10 16:12:26 -0300

    Removidos comentários e coisas desnecssárias.

--- app.py
+++ app.py
@@ -1,14 +1,12 @@
-import re
 from datetime import datetime, timedelta
 from flask import Flask, request, jsonify, make_response
 from flask_sqlalchemy import SQLAlchemy
-from marshmallow import fields, Schema, ValidationError, validates
+from marshmallow import fields, Schema, ValidationError
 from marshmallow.validate import Length, And, Regexp, Range
 from flask_migrate import Migrate
 
 
 app = Flask(__name__)
-# os.environ['FLASK_APP'] = 'app'  # a princípio não é necessário
 
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://gabriel:senha@db:3306/desafioflex'  # quando usa docker compose/mysql
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://gabriel:senha@127.0.0.1:3306/desafioflex'  # quando usa mysql
@@ -80,17 +78,10 @@ class CertificateSchema(Schema):
     created_at = fields.DateTime(dump_only=True)
     updated_at = fields.DateTime(dump_only=True)
     expiration_at = fields.DateTime()
-    # username = fields.Str(required=True, validate=Length(max=30))
     username = fields.Str(required=True, validate=And(Length(max=255), Regexp(regex=r'^[a-zA-Z0-9]+$')))
     name = fields.Str(required=True, validate=Length(max=255))
     expiration = fields.Integer(required=True, validate=Range(10, 3650))
-    groups = fields.Method("get_groups_ids", "load_groups_ids")  # esse funciona, quando não ta validando (.load(data, INCLUDE))
-
-    # @validates("username")
-    # def validate_username(self, value):
-    #     pattern = r'^[a-zA-Z0-9]+$'
-    #     if not re.match(pattern, value):
-    #         raise ValidationError("the username must only contain alphanumeric characters")
+    groups = fields.Method("get_groups_ids", "load_groups_ids")
 
     class Meta:
         fields = (
@@ -107,13 +98,6 @@ class CertificateSchema(Schema):
         )
 
 
-# certificate_schema = CertificateSchema()
-# certificates_schema = CertificateSchema(many=True)
-# group_schema = GroupSchema()
-# groups_schema = GroupSchema(many=True)
-
-
-# Rotas da API
 @app.route('/certificados', methods=['GET'])
 def list_certificates():
 
@@ -156,35 +140,14 @@ def create_certificate():
     with app.app_context():
 
         try:
-            # certificate_data = CertificateSchema().load(request.json, unknown=INCLUDE)
             certificate_data = CertificateSchema().load(request.json)
         except ValidationError as err:
             print(f"Erro no create_certificate: {err.messages}")
             print(f"Campos ok no create_certificate: {err.data}")
             return make_response(jsonify(err.messages), 400)
 
-        # certificate_data = request.json
         group_ids = [int(group_id) for group_id in certificate_data['groups']]
         groups = Group.query.filter(Group.id.in_(group_ids)).all()
-
-        # username = certificate_data['username']
-        # pattern = r'^[a-zA-Z0-9]+$'
-        # if not re.match(pattern, username):
-        #     return make_response(jsonify({'message': 'the username must only contain alphanumeric characters'}), 400)
-
-        # if len(username) > 30:
-        #     return make_response(jsonify({'message': 'the username must not exceed 30 characters'}), 400)
-
-        # existing_certificate = Certificate.query.filter_by(username=username).first()
-        # if existing_certificate:
-        #     return make_response(jsonify({'message': 'username already exists.'}), 400)
-
-        # expiration = certificate_data['expiration']
-        # if isinstance(expiration, int):
-        #     if expiration > 3650 or expiration < 10:
-        #         return make_response(jsonify({'message': 'the expiration field must be an integer between 10 and 3650'}), 400)
-        # else:
-        #     return make_response(jsonify({'message': 'the expiration field must be an integer'}), 400)
 
         certificate = Certificate(
             name=certificate_data['name'],
@@ -221,7 +184,7 @@ def create_group():
 @app.route('/certificados/<int:cert_id>', methods=['GET'])
 def get_certificate(cert_id):
     try:
-        certificate = db.session.get(Certificate, cert_id)  # = Certificate.query.get(cert_id)
+        certificate = db.session.get(Certificate, cert_id)
 
         if certificate:
             certificate_serialized = CertificateSchema(many=False).dump(certificate)
@@ -236,7 +199,7 @@ def get_certificate(cert_id):
 @app.route('/grupos/<int:group_id>', methods=['GET'])
 def get_group(group_id):
     try:
-        group = db.session.get(Group, group_id)  # = Group.query.get(group_id)
+        group = db.session.get(Group, group_id)
 
         if group:
             group_serialized = GroupSchema(many=False).dump(group)
@@ -252,10 +215,9 @@ def get_group(group_id):
 @app.route('/certificados/<int:cert_id>', methods=['PUT'])
 def update_certificate(cert_id):
     try:
-        certificate = db.session.get(Certificate, cert_id)  # = Certificate.query.get(cert_id)
+        certificate = db.session.get(Certificate, cert_id)
 
         if certificate:
-            # data = request.get_json()
             data = request.json
             certificate.username = data['username']
             certificate.name = data['name']
@@ -274,7 +236,7 @@ def update_certificate(cert_id):
 @app.route('/grupos/<int:group_id>', methods=['PUT'])
 def update_group(group_id):
     try:
-        group = db.session.get(Group, group_id)  # = Group.query.get(group_id)
+        group = db.session.get(Group, group_id)
 
         if group:
 
@@ -305,7 +267,7 @@ def update_group(group_id):
 @app.route('/certificados/<int:cert_id>', methods=['DELETE'])
 def delete_certificate(cert_id):
     try:
-        certificate = db.session.get(Certificate, cert_id)  # = Certificate.query.get(cert_id)
+        certificate = db.session.get(Certificate, cert_id)
         if certificate:
             db.session.delete(certificate)
             db.session.commit()
@@ -319,7 +281,7 @@ def delete_certificate(cert_id):
 @app.route('/grupos/<int:group_id>', methods=['DELETE'])
 def delete_group(group_id):
     try:
-        group = db.session.get(Group, group_id)  # = Group.query.get(group_id)
+        group = db.session.get(Group, group_id)
         if group:
             db.session.delete(group)
             db.session.commit()
@@ -331,5 +293,5 @@ def delete_group(group_id):
 
 
 if __name__ == "__main__":
-    db.create_all() # necessário adicionar se for utilizar o SQLite3
+    db.create_all()  # necessário adicionar se for utilizar o SQLite3
     app.run(debug=True)
